28a36bd09ef74d01d36a75fd074d537129e302a6
mohanstar <98344550+mohanstar@users.noreply.github.com>
2023-05-20 15:09:32 +0530

    Update calculator.groovy

--- vars/calculator.groovy
+++ vars/calculator.groovy
@@ -5,8 +5,8 @@ def mul(x,y){
 echo "multiplication of ${x} and ${y} is ${x+y}"
 }
 def minus(x,y){
-echo "sum of ${x} and ${y} is ${x-y}"
+echo "substraction of ${x} and ${y} is ${x-y}"
 }
 def div(x,y){
-echo "sum of ${x} and ${y} is ${x%y}"
+echo "division of ${x} and ${y} is ${x%y}"
 }
